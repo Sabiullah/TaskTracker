ffc7ec08790537a8b13b6ad784b7619f438f9077
Sabiullah <safycosting@gmail.com>
2026-04-25 22:46:18 +0530

    style(working_days): align admin + apps with sibling-app conventions

--- core/working_days/admin.py
+++ core/working_days/admin.py
@@ -5,6 +5,8 @@ from .models import WorkingDayOverride
 
 @admin.register(WorkingDayOverride)
 class WorkingDayOverrideAdmin(admin.ModelAdmin):
-    list_display = ("date", "org", "is_working", "note", "created_by")
-    list_filter = ("org", "is_working")
-    search_fields = ("note",)
+    list_display = ["uid", "date", "org", "is_working", "note", "created_by"]
+    list_filter = ["org", "is_working"]
+    search_fields = ["note"]
+    readonly_fields = ["uid", "created_at", "updated_at"]
+    date_hierarchy = "date"
--- core/working_days/apps.py
+++ core/working_days/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class WorkingDaysConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "core.working_days"
     label = "working_days"
     verbose_name = "working day overrides"
