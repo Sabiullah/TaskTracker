ad6666143aa82ce09d9b8716370cbf55d5356597
Sabiullah <safycosting@gmail.com>
2026-04-29 21:34:00 +0530

    feat(kaizen): register Kaizen in Django admin

--- core/kaizen/admin.py
+++ core/kaizen/admin.py
@@ -1,1 +1,13 @@
-# Implemented in subsequent plan tasks.
+from django.contrib import admin
+
+from .models import Kaizen
+
+
+@admin.register(Kaizen)
+class KaizenAdmin(admin.ModelAdmin):
+    list_display = ["uid", "raised_by", "client", "area", "status", "entry_date"]
+    list_filter = ["status"]
+    search_fields = ["area", "description", "takeaway"]
+    autocomplete_fields = ["raised_by", "client", "reviewed_by"]
+    readonly_fields = ["uid", "created_at", "updated_at"]
+    date_hierarchy = "entry_date"
